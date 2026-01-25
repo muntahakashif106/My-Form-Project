1ff37de60b7164eecbaa0390fecc6ba33a79bb7c
Muntaha Kashif <muntahakashif106@gmail.com>
2026-01-25 08:07:24 +0500

    Add calculator files

---
+++ primitive.js
@@ -0,0 +1,17 @@
+// nn bb ss u 
+let a = null;  //primitive in js
+let b = 345;
+let c = true; // can also be false
+let d = BigInt("567")
+let e = symbol("i am a nice symbol")
+let g = undefined
+console.log(a, b, c, d, e, f, g)
+
+//object in js (non -primitive)
+const item = {
+    "harry": true,
+    "newh":false,
+    "lovish":67,
+    "rohan": undefined
+}
+console.log(item["rohan"])
